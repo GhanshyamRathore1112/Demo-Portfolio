4aa99a2b48d42b58f32f9e83860b853f87182f50
Ghanshyam Rathore <79785313+GhanshyamRathore1112@users.noreply.github.com>
2023-04-22 11:21:20 +0530

    Updated About section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -22,7 +22,7 @@ const About = () =>{
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat duis aute irure dolor in reprehende.
+          aliquip ex ea commodo consequat duis aute irure dolor.
         </p>
         <div className="a-award">
             <img src={Award} className="a-award-img" />
